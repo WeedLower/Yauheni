d16a88e41cd3608d90e26694cdf7ad8d49953a23
Евгений Бузовский <zhenyabuzovskii@gmail.com>
2021-10-29 20:01:29 +0300

    data

--- MyBookShopApp/src/main/resources/authors.sql
+++ MyBookShopApp/src/main/resources/authors.sql
@@ -1,29 +1,29 @@
 
-insert into authors (first_name, last_name) values ('Drucie', 'Goodbourn');
-insert into authors (first_name, last_name) values ('Marcia', 'Aggiss');
-insert into authors (first_name, last_name) values ('Giovanni', 'Bigg');
-insert into authors (first_name, last_name) values ('Duffie', 'Eynon');
-insert into authors (first_name, last_name) values ('Anselma', 'McKennan');
-insert into authors (first_name, last_name) values ('Gert', 'Kira');
-insert into authors (first_name, last_name) values ('Clerc', 'Djakovic');
-insert into authors (first_name, last_name) values ('Stacee', 'Elsy');
-insert into authors (first_name, last_name) values ('Dorotea', 'Bonhill');
-insert into authors (first_name, last_name) values ('Minni', 'Bunney');
-insert into authors (first_name, last_name) values ('Berty', 'Gandley');
-insert into authors (first_name, last_name) values ('Raynard', 'Tesdale');
-insert into authors (first_name, last_name) values ('Joanna', 'Hutfield');
-insert into authors (first_name, last_name) values ('Dannie', 'Glidder');
-insert into authors (first_name, last_name) values ('Timmie', 'Adshede');
-insert into authors (first_name, last_name) values ('Fernando', 'Neads');
-insert into authors (first_name, last_name) values ('Teddy', 'Guilliland');
-insert into authors (first_name, last_name) values ('Paloma', 'Grebert');
-insert into authors (first_name, last_name) values ('Kristos', 'Usher');
-insert into authors (first_name, last_name) values ('Darcey', 'Rohmer');
-insert into authors (first_name, last_name) values ('Cristie', 'Isacsson');
-insert into authors (first_name, last_name) values ('Lindsay', 'Coutthart');
-insert into authors (first_name, last_name) values ('Helyn', 'Schneidar');
-insert into authors (first_name, last_name) values ('Bette-ann', 'Askem');
-insert into authors (first_name, last_name) values ('Dom', 'Robuchon');
+insert into authors (id,first_name, last_name) values (1,'Drucie', 'Goodbourn');
+insert into authors (id,first_name, last_name) values (2,'Marcia', 'Aggiss');
+insert into authors (id,first_name, last_name) values (3,'Giovanni', 'Bigg');
+insert into authors (id,first_name, last_name) values (4,'Duffie', 'Eynon');
+insert into authors (id,first_name, last_name) values (5,'Anselma', 'McKennan');
+insert into authors (id,first_name, last_name) values (6,'Gert', 'Kira');
+insert into authors (id,first_name, last_name) values (7,'Clerc', 'Djakovic');
+insert into authors (id,first_name, last_name) values (8,'Stacee', 'Elsy');
+insert into authors (id,first_name, last_name) values (9,'Dorotea', 'Bonhill');
+insert into authors (id,first_name, last_name) values (10,'Minni', 'Bunney');
+insert into authors (id,first_name, last_name) values (11,'Berty', 'Gandley');
+insert into authors (id,first_name, last_name) values (12,'Raynard', 'Tesdale');
+insert into authors (id,first_name, last_name) values (13,'Joanna', 'Hutfield');
+insert into authors (id,first_name, last_name) values (14,'Dannie', 'Glidder');
+insert into authors (id,first_name, last_name) values (15,'Timmie', 'Adshede');
+insert into authors (id,first_name, last_name) values (16,'Fernando', 'Neads');
+insert into authors (id,first_name, last_name) values (17,'Teddy', 'Guilliland');
+insert into authors (id,first_name, last_name) values (18,'Paloma', 'Grebert');
+insert into authors (id,first_name, last_name) values (19,'Kristos', 'Usher');
+insert into authors (id,first_name, last_name) values (20,'Darcey', 'Rohmer');
+insert into authors (id,first_name, last_name) values (21,'Cristie', 'Isacsson');
+insert into authors (id,first_name, last_name) values (22,'Lindsay', 'Coutthart');
+insert into authors (id,first_name, last_name) values (23,'Helyn', 'Schneidar');
+insert into authors (id,first_name, last_name) values (24,'Bette-ann', 'Askem');
+insert into authors (id,first_name, last_name) values (25,'Dom', 'Robuchon');
 
 
 
--- MyBookShopApp/src/main/resources/schema.sql
+++ MyBookShopApp/src/main/resources/schema.sql
@@ -1,29 +1,16 @@
 DROP TABLE IF EXISTS authors;
 DROP TABLE IF EXISTS books;
 
--- create table authors (
---                          id INT primary key ,
---                          first_name VARCHAR(50),
---                          last_name VARCHAR(50)
--- );
-
 CREATE TABLE authors(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    authorID VARCHAR(250) NOT NULL
+    id INT ,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
 );
 
 CREATE TABLE  books(
 id INT AUTO_INCREMENT PRIMARY KEY,
-authorID INT FOREIGN KEY REFERENCES authors(id),
 title VARCHAR(250) NOT NULL,
 priceOld  VARCHAR(250) DEFAULT NULL,
-price VARCHAR(250) DEFAULT NULL
+price VARCHAR(250) DEFAULT NULL,
+author_id INT FOREIGN KEY REFERENCES authors(id)
 );
-
--- CREATE TABLE  books(
---                        id INT AUTO_INCREMENT PRIMARY KEY,
---                        author VARCHAR(250) foreign key references authors(first_name, last_name) ,
---                        title VARCHAR(250) NOT NULL,
---                        priceOld  VARCHAR(250) DEFAULT NULL,
---                        price VARCHAR(250) DEFAULT NULL
--- );
